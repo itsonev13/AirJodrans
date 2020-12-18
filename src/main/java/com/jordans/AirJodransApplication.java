2bdb08500de2a7c22d6a59d49ac7131c8fadb7fb
Ivan Tsonev <i_tsonev@abv.bg>
2020-12-18 08:57:30 +0200

    Initial commit

---
+++ src/main/java/com/jordans/AirJodransApplication.java
@@ -0,0 +1,13 @@
+package com.jordans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirJodransApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirJodransApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jordans/AirJodransApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirJodransApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
